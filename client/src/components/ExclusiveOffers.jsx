e93fd9df307a631f30c69b28562ad8905c11b27e
sarim-alis <sarimslayerali786@gmail.com>
2026-05-10 08:47:35 -0700

    fea: exc;usive

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -16,7 +16,7 @@ const ExclusiveOffers = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
                 {exclusiveOffers.map((item) => (
-                    <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{ backgroundImage: `url(${item.image})` }}>
+                    <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center transition-all duration-300 hover:-translate-y-2 hover:scale-105 hover:shadow-xl' style={{ backgroundImage: `url(${item.image})` }}>
                         <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>
                             {item.priceOff}% OFF
                         </p>
