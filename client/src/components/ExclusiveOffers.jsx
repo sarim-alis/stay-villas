070fcb4235b429fc7408da166c694de2ad0fbd2e
sarim-alis <sarimslayerali786@gmail.com>
2026-05-10 08:48:35 -0700

    feats

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -8,7 +8,7 @@ const ExclusiveOffers = () => {
         <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20'>
             <div className='flex flex-col md:flex-row items-center justify-between w-full '>
                 <Title align='left' title="Exclusive Offers" subTitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories." />
-                <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12'>
+                <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12 border border-gray-300 px-4 py-2 rounded-xl hover:bg-gray-50 transition-all'>
                     View All Offers
                     <img className='group-hover:translate-x-1 transition-all' src={assets.arrowIcon} alt="arrow-icon" />
                 </button>
