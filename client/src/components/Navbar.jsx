856cc109957d8c46f05829d2b642eeff31473a23
sarim-alis <sarimslayerali786@gmail.com>
2026-05-11 05:54:58 -0700

    feats

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
     const navLinks = [
         { name: 'Home', path: '/' },
         { name: 'Hotels', path: '/rooms' },
-        { name: 'Experience', path: '/' },
-        { name: 'About', path: '/' },
+        // { name: 'Experience', path: '/' },
+        // { name: 'About', path: '/' },
     ];
 
     const [isScrolled, setIsScrolled] = useState(false);
