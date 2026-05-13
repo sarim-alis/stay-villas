07dcf5d71dec2be36b91e41fa5f8c8d374a3f417
sarim-ali-sam <sarim@strivio.ai>
2026-05-12 21:45:12 -0700

    rem: owner check

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -6,13 +6,13 @@ import { useAppContext } from '../../context/AppContext'
 
 const Layout = () => {
 
-    const { isOwner, navigate } = useAppContext()
+    // const { isOwner, navigate } = useAppContext()
 
-    useEffect(() => {
-        if (!isOwner) {
-            navigate('/')
-        }
-    }, [isOwner])
+    // useEffect(() => {
+    //     if (!isOwner) {
+    //         navigate('/')
+    //     }
+    // }, [isOwner])
 
     return (
         <div className='flex flex-col h-screen'>
