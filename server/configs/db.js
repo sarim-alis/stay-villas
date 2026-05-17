e41354e09a5b8875d99e2fe1d0158e47f02656a7
sarim-alis <sarimslayerali786@gmail.com>
2026-05-17 13:33:21 -0700

    feat: db

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
     try {
         mongoose.connection.on('connected', () => console.log("Mango connected 🥭🍭⭐🍊🔰"));
         await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
