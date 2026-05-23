79dac0d3ba1a7f02b316cb05669be2b9b2946938
sarim-alis <sarimslayerali786@gmail.com>
2026-05-23 10:23:13 -0700

    feat: featured

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -9,7 +9,7 @@ const FeaturedDestination = () => {
     return rooms.length > 0 && (
         <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
             <Title title="Featured Destination" subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences." />
-            <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-20 w-full max-w-5xl'>
                 {rooms.slice(0, 4).map((room, index) => (
                     <HotelCard key={room._id} room={room} index={index} />
                 ))}
